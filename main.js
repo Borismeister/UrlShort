d99ee786c6e96a0bb5245429b41ce0552e909cfa
Max Zadarnovskiy <40841820+Borismeister@users.noreply.github.com>
2020-09-09 20:26:38 +0930

    complete

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ const morgan = require('morgan')
 const { Client } = require('pg');
 
 const client = new Client({
-    user: 'postgres',
+    user: 'Max',
     host: 'localhost',
-    database: 'testdb',
-    password: 'Zadarnovskiy1',
+    database: 'urls',
+    password: 'pass1234',
     port: 5432,
 });
 
@@ -24,23 +24,40 @@ function getrandom(){
   return random_string
 }
 
-app.get('/', (req, res) => {
-  console.log(req.query)
-  res.sendStatus(200)
-})
-
 app.get('/generate', (req, res)=>{
   let short = getrandom()
+  let long = req.query.url;
+  let time = new Date().getTime();
 
-
-
+  const text = `insert into short_urls(short, long, time_to_delete) values($1, $2, $3)`;
+  const values = [short, long, time + 259200000];
+  
+  client
+  .query(text, values)
+  .then(res => {
+    console.log(res)
+  })
+  .catch(e => console.error(e.stack))
   res.json({
     url: short
   })
 })
 
-app.get('/:id', (req, res)=>{
-  res.send(req.params.id)
+app.get('/:short', (req, res)=>{
+
+  let text = `select long from short_urls where short = $1`
+  let values = [req.params.short]
+
+  client.query(text, values)
+    .then(resp => {
+      console.log(resp.rows[0].long)
+      // res.json({
+      //   short: req.params.short,
+      //   long: resp.rows[0].long
+      // })
+      res.redirect(resp.rows[0].long)
+    })
+    .catch(e => console.error(e.stack))
 })
 
 app.listen('4000', ()=>{
